c1c72c0e3686c56fadaf9800928134c49e98ca79
Dani Roxberry <dani@bitoriented.com>
2018-10-03 14:55:06 -0700

    [add] process.env.PORT

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +16,7 @@ app.set("view engine", "handlebars");
 
 const reviews = require("./controllers/reviews")(app);
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("App listening on port 3000!");
 });
 
