c3aa0836e70a5f4b267ce34448de2405966e200f
Dani Roxberry <dani@bitoriented.com>
2018-10-03 13:56:30 -0700

    [add] step 5

--- app.js
+++ app.js
@@ -37,11 +37,21 @@ app.get("/reviews/new", (req, res) => {
   res.render("reviews-new", {});
 });
 
+app.get("/reviews/:id", (req, res) => {
+  Review.findById(req.params.id)
+    .then(review => {
+      res.render("reviews-show", { review: review });
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
+
 app.post("/reviews", (req, res) => {
   Review.create(req.body)
     .then(review => {
       console.log(review);
-      res.redirect("/");
+      res.redirect(`/reviews/${review._id}`); // Redirect to reviews/:id
     })
     .catch(err => {
       console.log(err.message);
