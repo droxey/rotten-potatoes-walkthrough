311c687d8cedf58bfe5b2d8e9d4871e3d01ad57c
Dani Roxberry <dani@bitoriented.com>
2018-10-03 14:37:32 -0700

    [add] tutorial step 7 complete

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const methodOverride = require("method-override");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
 const app = express();
 
 app.use(methodOverride("_method"));
@@ -14,79 +13,10 @@ mongoose.connect(
   { useMongoClient: true }
 );
 
-const Review = mongoose.model("Review", {
-  title: String,
-  description: String,
-  movieTitle: String
-});
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// OUR MOCK ARRAY OF PROJECTS
-let reviews = [{ title: "Great Review" }, { title: "Next Review" }];
-
-app.get("/", (req, res) => {
-  Review.find()
-    .then(reviews => {
-      res.render("reviews-index", { reviews: reviews });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.get("/reviews/new", (req, res) => {
-  res.render("reviews-new", {});
-});
-
-app.get("/reviews/:id", (req, res) => {
-  Review.findById(req.params.id)
-    .then(review => {
-      res.render("reviews-show", { review: review });
-    })
-    .catch(err => {
-      console.log(err.message);
-    });
-});
-
-app.get("/reviews/:id/edit", (req, res) => {
-  Review.findById(req.params.id, function(err, review) {
-    res.render("reviews-edit", { review: review });
-  });
-});
-
-app.post("/reviews", (req, res) => {
-  Review.create(req.body)
-    .then(review => {
-      console.log(review);
-      res.redirect(`/reviews/${review._id}`); // Redirect to reviews/:id
-    })
-    .catch(err => {
-      console.log(err.message);
-    });
-});
-
-app.put("/reviews/:id", (req, res) => {
-  Review.findByIdAndUpdate(req.params.id, req.body)
-    .then(review => {
-      res.redirect(`/reviews/${review._id}`);
-    })
-    .catch(err => {
-      console.log(err.message);
-    });
-});
-
-app.delete("/reviews/:id", function(req, res) {
-  console.log("DELETE review");
-  Review.findByIdAndRemove(req.params.id)
-    .then(review => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      console.log(err.message);
-    });
-});
+const reviews = require("./controllers/reviews")(app);
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
