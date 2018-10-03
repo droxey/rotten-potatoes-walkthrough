67d513f6d98208ef5e3c2c50bd14285f92b3d15e
Dani Roxberry <dani@bitoriented.com>
2018-10-03 15:30:25 -0700

    [add] last part of tutorial

--- controllers/comments.js
+++ controllers/comments.js
@@ -11,4 +11,15 @@ module.exports = app => {
         console.log(err.message);
       });
   });
+
+  app.delete("/reviews/comments/:id", function(req, res) {
+    console.log("DELETE comment");
+    Comment.findByIdAndRemove(req.params.id)
+      .then(comment => {
+        res.redirect(`/reviews/${comment.reviewId}`);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  });
 };
