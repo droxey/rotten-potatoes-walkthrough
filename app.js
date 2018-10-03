d88805fe7075c32900cde6d487dba5ccf0af370b
Dani Roxberry <dani@bitoriented.com>
2018-10-03 13:52:13 -0700

    [add] tutorial step 4

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(
+  "mongodb://localhost/rotten-potatoes",
+  { useMongoClient: true }
+);
+
+const Review = mongoose.model("Review", {
+  title: String,
+  description: String,
+  movieTitle: String
+});
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [{ title: "Great Review" }, { title: "Next Review" }];
+
+app.get("/", (req, res) => {
+  Review.find()
+    .then(reviews => {
+      res.render("reviews-index", { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get("/reviews/new", (req, res) => {
+  res.render("reviews-new", {});
+});
+
+app.post("/reviews", (req, res) => {
+  Review.create(req.body)
+    .then(review => {
+      console.log(review);
+      res.redirect("/");
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000!");
+});
